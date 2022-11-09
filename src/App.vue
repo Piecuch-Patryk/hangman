62c226df10c5a67cea029893f93ab4c17385e7d0
Patryk <patrykowo@gmail.com>
2022-11-09 13:55:17 +0100

    Add setup-game component with content and mobile styles.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <transition name="fade">
+    <Setup @game-init="gameInit" v-show="showSetup" :categories="categories" :difficulty="difficulty" />
+  </transition>
+  <Game :data="categories" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Game from './components/Game.vue'
+import Setup from './components/Setup.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Setup,
+    Game,
+  },
+  data() {
+    return {
+      categories: {
+        movie: {
+          1: {
+            title: 'The Hunger Games',
+            hint: 'Every year in the ruins of what was once North America, the nation of Panem forces each of its twelve districts to send a teenage boy and girl to compete in...',
+          },
+          2: {
+            title: 'Idiocracy',
+            hint: 'Top-secret Human Hibernation Project. Pentagon picks the most average Americans they can find - private soldier and a prostitute and send them to the year 2505',
+          }
+        },
+        car: {
+          1: {
+            title: 'Audi',
+            hint: 'German automobile manufacturer that designs, engineers, produces, markets and distributes luxury vehicles. Member of the Volkswagen Group.',
+          },
+          2: {
+            title: 'Ford',
+            hint: 'American multinational automaker that has its main headquarters in Dearborn, Michigan, a suburb of Detroit.',
+          }
+        },
+      },
+      difficulty: {
+        easy: 10,
+        medium: 5,
+        hard: 2,
+      },
+      showSetup: true,
+    }
+  },
+  methods: {
+    gameInit(category, level) {
+      console.log(category)
+      console.log(level)
+      this.showSetup = false
+    },
   }
 }
 </script>
@@ -19,8 +62,16 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
---
+++ src/components/Setup.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="setup">
+    <div class="inner">
+      <div>
+        <h2>Game Setup</h2>
+        <p>Choose category and difficulty level from the options below.</p>
+        <p>You've got to guess it, you'll hang otherwise.</p>
+      </div>
+      <div>
+        <h3>Choose Category</h3>
+        <label v-for="(value, key, index) in categories" :key="index">
+          <input v-model="category" type="radio" :value="value" name="category">
+          {{ key }}
+        </label>
+      </div>
+      <div>
+        <h3>Choose Difficulty Level</h3>
+        <label v-for="(value, key, index) in difficulty" :key="index">
+          <input v-model="level" type="radio" :value="value" name="difficulty">
+          {{ key }}
+        </label>
+      </div>
+      <div>
+        <transition name="fade">
+          <button v-show="category !== '' && level !== ''" @click="$emit('game-init', category, level)">Start Game</button>
+        </transition>
+      </div>
+    </div>
+    <div class="img-wrapper">
+      <img src="@/assets/game/gibbet10.png" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Setup',
+  props: {
+    categories: Object,
+    difficulty: Object,
+  },
+  data() {
+    return {
+      category: '',
+      level: '',
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.setup {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #fff;
+  text-align: center;
+
+  .inner {
+    background-color: #662d91;
+    padding: 1rem;
+    color: #ddd;
+    
+    h2 {
+      font-size: 2.5rem;
+      margin: 1rem auto;
+    }
+
+    label {
+      text-transform: uppercase;
+      margin: 0 1rem;
+    }
+
+    button {
+      background-color: #fff;
+      padding: .3rem 1rem;
+      color: #333;
+      border: none;
+      border-radius: .5rem;
+      margin: 2rem auto;
+      font-size: 1.3rem;
+    }
+  }
+
+  .img-wrapper {
+    img {
+      width: 100%;
+    }
+  }
+}
+</style>
